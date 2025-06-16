b0c6546e994f412aed6fa273a099f8e0e6898cfd
prahlad chaudhary <prahladchaudhary@Prahlads-MacBook.local>
2025-06-16 17:07:16 +0530

    blog images uploaded on imagekit, && APIs created for blog list, details, delete and updation

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import cors from 'cors'
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
 
 
 
@@ -17,11 +18,12 @@ app.use(express.json())
 //Routes
 app.get('/', (req, res)=> res.send("API is Working"))
 app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on port' + PORT)
+    console.log('Server is running on port ' + PORT)
 })
 
 export default app;
